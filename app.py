70c0af13a97977ef52a42c279433e12261463cfc
computer <nitinchaudhary0652@gmail.com>
2024-12-24 22:12:26 +0530

    add python code

---
+++ app.py
@@ -0,0 +1,17 @@
+# num1=int(input("Enter first number :"))
+# num2=int(input("Enter second number :"))
+# num3=int(input("Enter third number :"))
+
+# if(num1>num2 and num1>num3):
+#     print("Num1 is grater than num2 & num3 :",num1)
+
+# elif(num2>num1 and num2>num3):
+#     print("Num2 is greater than num1 & num3 :",num2)
+
+# else:
+#     print("Num3 is greater than num1 & num3 :",num3)
+
+
+arr=[0,1,"arjun",10]
+arr.pop()
+print(arr)
